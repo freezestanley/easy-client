5f6d0806e344d87c891725e8009a3ff80c02eeff
stanleyxu <stanleyxu@stanleyxudeMacBook-Pro.local>
2019-11-10 19:27:22 +0800

    xx

---
+++ packages/easy-plugins-babel/index.js
@@ -0,0 +1,25 @@
+// module
+module.exports = {
+    configureWebpack: config => {
+        config.module.rule('compile')
+        .exclude.add(/(node_modules|bower_components)/)
+        .end()
+        .include.add(path.resolve(cwd, './src')).end()
+        .test(/\.js|jsx|mjs$/)
+        .use('babel')
+        .loader('babel-loader').options({
+            "presets": ["@babel/preset-env", "@babel/preset-react"],
+            "plugins": [
+                "@babel/plugin-transform-runtime",
+                "@babel/plugin-proposal-class-properties",
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/plugin-transform-react-jsx-self"
+            ]
+        }).end()
+        .use('eslint-loader')
+        .loader('eslint-loader')
+        .options({
+            formatter: require('eslint-friendly-formatter')
+        }).end()
+    }
+}

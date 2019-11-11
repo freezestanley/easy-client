b9c52136628be030dd27f34beb7699b66c6df8e0
stanleyxu <stanleyxu@stanleyxudeMacBook-Pro.local>
2019-11-11 19:36:10 +0800

    xx

--- packages/easy-plugins-react/index.js
+++ packages/easy-plugins-react/index.js
@@ -4,6 +4,11 @@ const path = require('path')
 
 module.exports = {
     configureWebpack: cfg => {
+        cfg.addDependencies([
+            {"react": "^16.11.0"},
+            {"react-dom": "^16.11.0"},
+            {"react-router-dom": "^5.1.2"}
+        ])
         let config = cfg.config
         // devtool
         config.devtool('cheap-module-eval-source-map')

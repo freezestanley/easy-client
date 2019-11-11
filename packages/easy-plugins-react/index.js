f26bbbc8af02bd30d7a781c84954a93a09b56c48
stanleyxu <stanleyxu@stanleyxudeMacBook-Pro.local>
2019-11-11 19:07:53 +0800

    xx

--- packages/easy-plugins-react/index.js
+++ packages/easy-plugins-react/index.js
@@ -1,6 +1,12 @@
+'use strict'
+const webpack = require('webpack')
+const path = require('path')
+
 module.exports = {
     configureWebpack: cfg => {
         let config = cfg.config
-
+        // devtool
+        config.devtool('cheap-module-eval-source-map')
+        
     }
 }
